08ce00d927446b0be44d2fbab9dfc87890ee23fd
fgw1027 <fgw1027@gmail.com>
2024-09-16 17:13:16 +0800

    Add sort

--- main.cpp
+++ main.cpp
@@ -2,15 +2,24 @@
 
 using namespace std;
 
-void sort(int[] arr,int size)
+void sort(int arr[],int size)
 {
-
+	int tmp=0;
+	for(int i=0;i<size-1;++i)
+	{
+		for(int j=0;j<size-1-i;++j)
+			if (arr[j]>arr[j+1])
+			{
+				tmp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=tmp;
+			}	
+	}
 }
-
-int main()
-{
-	int arr[]={10,8,3,4,7,98,1};
-	int size=sizeof(arr)/sizeof(int);
-	sort(arr,size);
-	return 0;
-}
+	int main()
+	{
+		int arr[]={10,8,3,4,7,98,1};
+		int size=sizeof(arr)/sizeof(int);
+		sort(arr,size);
+		return 0;
+	}
