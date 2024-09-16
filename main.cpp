869ef8687296c7a5ac387bd48a5843210f99084c
fgw1027 <fgw1027@gmail.com>
2024-09-16 17:22:39 +0800

    adjust format

--- main.cpp
+++ main.cpp
@@ -16,10 +16,10 @@ void sort(int arr[],int size)
 			}	
 	}
 }
-	int main()
-	{
-		int arr[]={10,8,3,4,7,98,1};
-		int size=sizeof(arr)/sizeof(int);
-		sort(arr,size);
-		return 0;
-	}
+int main()
+{
+	int arr[]={10,8,3,4,7,98,1};
+	int size=sizeof(arr)/sizeof(int);
+	sort(arr,size);
+	return 0;
+}
