7cfd2c5346c10fb6335290f3e7f30a40fedcbe72
fgw <fgw1027@gmail.com>
2024-09-16 19:31:32 +0800

    from small to big

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void sort(int arr[],int size)
 	for(int i=0;i<size-1;++i)
 	{
 		for(int j=0;j<size-1-i;++j)
-			if (arr[j]>arr[j+1])
+			if (arr[j]<arr[j+1])
 			{
 				tmp=arr[j];
 				arr[j]=arr[j+1];
