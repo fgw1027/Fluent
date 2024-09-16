acb4047e2868f5465695913ec60e6b351dfe19b7
fgw1027 <fgw1027@gmail.com>
2024-09-16 17:00:14 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void sort(int[] arr,int size)
+{
+
+}
+
+int main()
+{
+	int arr[]={10,8,3,4,7,98,1};
+	int size=sizeof(arr)/sizeof(int);
+	sort(arr,size);
+	return 0;
+}
