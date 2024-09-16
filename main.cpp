4e03f7598515b4c886b2f570defa1504df121d9d
fgw <fgw1027@gmail.com>
2024-09-16 23:09:40 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,52 +10,6 @@ void swap(int& a, int& b) {
 }
 
 // 分区函数
-int partition(vector<int>& arr, int low, int high) {
-    int pivot = arr[high]; // 选择最右边的元素作为枢轴
-    int i = low - 1; // i 表示小于枢轴的区域的最后一个元素的索引
-
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]); // 将小于枢轴的元素移动到左边
-        }
-    }
-    swap(arr[i + 1], arr[high]); // 将枢轴放到正确的位置
-    return i + 1; // 返回枢轴的位置
-}
-
-// 快速排序函数
-void quicksort(vector<int>& arr, int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high); // 分区点
-        quicksort(arr, low, pi - 1);  // 递归排序左半部分
-        quicksort(arr, pi + 1, high); // 递归排序右半部分
-    }
-}
-
-int main() {
-    vector<int> arr = {10, 7, 8, 9, 1, 5};  // 示例数组
-    int n = arr.size();
-
-    quicksort(arr, 0, n - 1); // 对整个数组进行快速排序
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "; // 输出排序后的数组
-    }
-    cout << endl;
-
-    return 0;
-}
-
-// 交换两个元素
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-// 分区函数
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];  // 选择最右边的元素作为枢轴
     int i = low - 1;        // i 表示小于枢轴的区域的最后一个元素的索引
